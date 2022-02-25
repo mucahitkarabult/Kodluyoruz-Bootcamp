37c80636a8b9a3d24adfbba3f8d1543a01f4f9b5
Mücahit Karabulut <76858679+mucahitkarabult@users.noreply.github.com>
2022-02-25 19:36:54 +0300

    ReadMe.

---
+++ week2/README.d
@@ -0,0 +1,5 @@
+# WEEK 2
+week_2_Scraping.ipynb is a simple web scraping script.
+You can change the number of page in code this way you can scrape more data.
+You can check the result of scraping in result.csv
+
